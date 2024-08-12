297822985ffe5077fa2b54f4f4082e82cb824f9d
Sophie07 <sphiaalvarez2507@gmail.com>
2024-08-12 15:40:04 -0400

    first commit :) & file transfer

---
+++ moneySystem.js
@@ -0,0 +1,38 @@
+let playerMoney = 100;
+let moneyText; 
+
+function loadMoney() {
+    const storedMoney = localStorage.getItem('playerMoney');
+    if (storedMoney) {
+        playerMoney = parseInt(storedMoney, 10);
+    }
+}
+
+
+function addMoney(amount) {
+    playerMoney += amount;
+    updateMoney();
+    saveMoney();
+}
+
+function subtractMoney(amount) {
+    if (playerMoney >= amount) {
+        playerMoney -= amount;
+        updateMoney();
+        saveMoney();
+    } else {
+        console.log("no money :(");
+    }
+}
+
+function updateMoney() {
+    if (moneyText) {
+        moneyText.setText('money: $' + playerMoney, { fontSize: '32px', fill: '#00000' });
+    }
+}
+
+
+function saveMoney () 
+    {
+    localStorage.setItem('playerMoney', playerMoney);
+    }
