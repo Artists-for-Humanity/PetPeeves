0ab4f4241757eab1df4aa85bf75a82edcdb746b5
Sophie07 <sphiaalvarez2507@gmail.com>
2024-08-13 11:40:58 -0400

    timer progress 1

--- timer.js
+++ timer.js
@@ -11,15 +11,14 @@ var timedEvent;
 
 var game = new Phaser.Game(config);
 
-function create ()
+function Timecreate ()
 {
   console.log('create');
-  // 2:30 in seconds
+
   this.initialTime = 60;
 
   text = this.add.text(32, 32, 'Day: ' + formatTime(this.initialTime));
 
-  // Each 1000 ms call onEvent
   timedEvent = this.time.addEvent({ delay: 1000, callback: onEvent, callbackScope: this, loop: true });
 }
 
