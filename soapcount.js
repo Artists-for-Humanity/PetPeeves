630c15ef137dadafca9a62187decf510a3a3289b
Sophie072 <sphiaalvarez2507@gmail.com>
2024-08-19 15:31:01 -0400

    Fixed soap (#6)

---
+++ soapcount.js
@@ -0,0 +1,38 @@
+let soapCount = 2;
+let soapText; 
+
+function storedSoap() {
+    const storedSoap = localStorage.getItem('soapCount');
+    if (storedSoap) {
+        soapCount = parseInt(storedSoap, 10);
+    }
+}
+
+
+function addSoap(amount) {
+    soapCount += amount;
+    updateSoap();
+    saveSoap();
+}
+
+function subtractSoap(amount) {
+    if (soapCount >= amount) {
+        soapCount -= amount;
+        updateSoap();
+        saveSoap();
+    } else {
+        console.log("no money :(");
+    }
+}
+
+function updateSoap() {
+    if (soapText) {
+        soapText.setText('Soap: ' + soapCount, { fontFamily: 'Arial Black', fontSize: '20px', fill: '#00000' });
+    }
+}
+
+
+function saveSoap () 
+    {
+    localStorage.setItem('soapCount', soapCount);
+    }
